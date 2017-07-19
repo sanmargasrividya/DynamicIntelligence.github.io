904700437a79afe1243e9fcf97fed9a641b3795c
sanmargasrividya <sindhurabandaru@gmail.com>
2017-07-19 08:01:45 +0530

    Add files via upload

--- magazine/js/bulbAnimation.js
+++ magazine/js/bulbAnimation.js
@@ -4,10 +4,12 @@ var bulbAnimationInterwel = null;
 		$('#light-bulb2').css({'opacity': '0'});	
 		bulbAnimationInterwel = setInterval(function(){
 			var opacityVal = $('#light-bulb2').css('opacity');
-			if(opacityVal == "1") opacityVal = '0';
-			else opacityVal = '1';
-			 $('#light-bulb2').css({'opacity': opacityVal});
-			 $('#light-bulb2').css({'opacity': opacityVal});
+			if(opacityVal == "1"){ 
+				 $('#light-bulb2').css({'opacity': '0'});
+			 	$('#light-bulb1').css({'opacity': '1'});}
+			else {
+				 $('#light-bulb2').css({'opacity': '1'});
+				 $('#light-bulb1').css({'opacity': '0'});}			
 			},500);
 	}
 	function removeBulbAnimation(){
